3b6b7b3f85610bb5f211b2876d8dc88c687f0278
shashank460 <shashankchurk2022@gmail.com>
2026-08-16 16:59:45 +0530

    ci: configure Node.js globals for ESLint

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,12 @@ export default defineConfig([
   eslint.configs.recommended,
   {
     ignores: ['node_modules/**'],
+    languageOptions: {
+      globals: {
+        console: 'readonly',
+        process: 'readonly'
+      }
+    },
     rules: {
       'no-unused-vars': ['error', { argsIgnorePattern: '^_' }]
     }
