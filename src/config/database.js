aeb8b0d48d8fbaf764c699402905ae10c9b261d8
shashank460 <shashankchurk2022@gmail.com>
2026-08-16 17:01:14 +0530

    test: validate database configuration only when starting the server

--- src/config/database.js
+++ src/config/database.js
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 import env from './env.js';
 
 export async function connectDatabase() {
+  if (!env.mongoUri) throw new Error('MONGODB_URI is required to start the server');
   await mongoose.connect(env.mongoUri);
   console.log('MongoDB connected');
 }
