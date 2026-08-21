a60a976554bf980cd975a51117a82ab83befcd63
shashank460 <shashankchurk2022@gmail.com>
2026-08-21 11:04:40 +0530

    fix: normalize attendance dates before persistence

--- src/models/Attendance.js
+++ src/models/Attendance.js
@@ -1,5 +1,12 @@
 import mongoose from 'mongoose';
 
+function normalizeToUtcMidnight(value) {
+  const date = new Date(value);
+  if (Number.isNaN(date.getTime())) return value;
+  date.setUTCHours(0, 0, 0, 0);
+  return date;
+}
+
 const attendanceSchema = new mongoose.Schema(
   {
     student: { type: mongoose.Schema.Types.ObjectId, ref: 'Student', required: true },
@@ -10,5 +17,9 @@ const attendanceSchema = new mongoose.Schema(
 );
 
 attendanceSchema.index({ student: 1, date: 1 }, { unique: true });
+
+attendanceSchema.pre('save', function normalizeDate() {
+  this.date = normalizeToUtcMidnight(this.date);
+});
 
 export default mongoose.model('Attendance', attendanceSchema);
